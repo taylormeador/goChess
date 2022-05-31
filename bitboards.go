258c31e695a171610e147491c4c0aabe2fd684b2
taylormeador <taylor.r.meador@gmail.com>
2022-05-31 08:13:17 -0600

    implement funcs for preserving game state

--- bitboards.go
+++ bitboards.go
@@ -14,3 +14,34 @@ var enPassantSquare = noSquare
 
 // castling rights
 var castle = -1
+
+// custom type for collecting board info
+type gameState struct {
+	bitboards       [12]uint64
+	occupancies     [3]uint64
+	enPassantSquare uint64
+	side            int
+	castle          int
+}
+
+// returns a copy of the gamestate
+func copyBoard() gameState {
+	var state gameState
+
+	state.bitboards = bitboards
+	state.occupancies = occupancies
+	state.enPassantSquare = enPassantSquare
+	state.side = side
+	state.castle = castle
+
+	return state
+}
+
+// sets the relevant global vars to reflect the given gamestate
+func restoreBoard(state gameState) {
+	bitboards = state.bitboards
+	occupancies = state.occupancies
+	enPassantSquare = state.enPassantSquare
+	side = state.side
+	castle = state.castle
+}
--- main.go
+++ main.go
@@ -12,14 +12,13 @@ func main() {
 	initAll()
 
 	parseFEN("r3k2r/pPppqpb1/bn2pnp1/3PN3/Pp2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq b5 0 1")
+	printBoard()
+	board := copyBoard()
 
-	for rank := uint64(0); rank < 8; rank++ {
-		for file := uint64(0); file < 8; file++ {
-			square := rank*8 + file
-			generateMoves(square)
-		}
-	}
-	printMoveList()
+	parseFEN(emptyBoard)
+	printBoard()
+
+	restoreBoard(board)
 	printBoard()
 
 }
