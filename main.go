b15654c52bb8a0b9bd7edda0dcc4f471e8aa21dc
taylormeador <taylor.r.meador@gmail.com>
2022-07-25 10:26:12 -0600

    implement perft test func

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 // init
 func initAll() {
 	initLeapersAttacks()
@@ -14,24 +9,7 @@ func initAll() {
 
 // main
 func main() {
-
 	initAll()
 	parseFEN(trickyPosition)
-	printBoard()
-	startTime := time.Now()
-	perftDriver(3)
-
-	//for _, move := range moveList {
-	//	copyBoard()
-	//	//input := bufio.NewScanner(os.Stdin)
-	//	//input.Scan()
-	//	if makeMove(move) == 0 {
-	//		continue
-	//	}
-	//	makeMove(move)
-	//	restoreBoard()
-	//}
-	endTime := time.Now()
-	fmt.Printf("%d nodes\n", nodes)
-	fmt.Printf("Time taken: %d", endTime.Sub(startTime))
+	perftTest(2)
 }
